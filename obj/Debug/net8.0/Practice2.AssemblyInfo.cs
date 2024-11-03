c7e7715d2867c76245c5b418081ccd2d2f028d80
Maria <202201175@alu.comillas.edu>
2024-11-03 11:23:03 +0100

    actualizacion archivos

--- obj/Debug/net8.0/Practice2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Practice2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Practice1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Practice2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27374dadb01fe269226f5644f80c512efe2ccf54")]
-[assembly: System.Reflection.AssemblyProductAttribute("Practice1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Practice1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Practice2")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Practice2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
